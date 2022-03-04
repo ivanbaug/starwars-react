53549035a447a97849751fc644b3ed062b41ac08
ivanbaug <ivsitioinfo@gmail.com>
2022-03-03 20:51:55 -0500

    build test

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,2 +1,3 @@
 export const SW_API_URL = 'https://swapi.dev/api/'
-export const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
+// export const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
+export const SECRET_KEY = "Star*Wars*SWAPI*-Test/2022-02-28"
